4a131c895732af7640795a30d2670e10f74d010e
Infiniterafa <197008946+DiegoJC27@users.noreply.github.com>
2025-07-14 09:49:14 -0600

    Jugador

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] int speed;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void RecibeDamage()
+    {
+    }
+}
