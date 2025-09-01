43ce5b7c54ad63a8007013e0d6e386dd0c0d2328
Diego <diego.cruzado@saei.mx>
2025-09-01 15:54:37 -0600

    Condision de victoria

--- Assets/GameFeelPlataformer/Scripts/MenuControler.cs
+++ Assets/GameFeelPlataformer/Scripts/MenuControler.cs
@@ -1,11 +1,14 @@
 using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
     [SerializeField] GameObject gameOverMenu;
     [SerializeField] GameObject gameWonMenu;
+    [SerializeField] TextMeshProUGUI coinsCollectedWon;
+    [SerializeField] TextMeshProUGUI coinsCollectedLose;
     public void ExitButton()
     {
 #if UNITY_EDITOR
@@ -26,12 +29,19 @@ public class MenuController : MonoBehaviour
 
     public void GameLost()
     {
+        GetCoinsCollected();
         gameOverMenu.SetActive(true);
-        Time.timeScale = 0;
     }
     public void GameWon()
     {
+        GetCoinsCollected();
         gameWonMenu.SetActive(true);
         Time.timeScale = 0;
+    }
+
+    private void GetCoinsCollected()
+    {
+        coinsCollectedLose.text = PlayerUIController.instance.mangosText.text;
+        coinsCollectedWon.text = PlayerUIController.instance.mangosText.text;
     }
 }
